32314e7d342ce9779c8ba469b6d14c378beabc30
Graeme Boy <graemelearn@protonmail.com>
2022-11-03 16:32:17 -0400

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,133 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import assert from 'assert';
+
+export const STACK_API_ENDPOINTS = {
+  TRACK: 'https://www.stack-so/api/v1/track',
+};
+
+export const STACK_CLIENT_ERRORS = {
+  NO_API_KEY_PROVIDED: 'StackClient: No API key provided',
+  INVALID_API_KEY: 'StackClient: Invalid API key provided',
+  NO_ACTIVITY_TYPE_PROVIDED: 'StackClient: Activity type is required',
+  INVALID_ACTIVITY_TYPE_PROVIDED: 'StackClient: Activity type must be a string',
+  INVALID_ACTIVITY_PROVIDED: 'StackClient: Invalid activity provided',
+  UNEXPECTED_SERVER_RESPONSE: 'StackClient: Unexpected server response',
+};
+
+export type ClientConfig = {
+  key: string;
+  network?: string;
+};
+
+export type Activity = {
+  actor?: ActivityObjectType;
+  object?: ActivityObjectType;
+};
+
+export interface ActivityObjectType {
+  address?: string;
+  network?: string;
+  id?: string | number;
+  type?: string;
+  transactionHash?: string;
+  extra?: any;
+}
+
+export type LoggableActivityType = {
+  type: string;
+  published: number;
+  network?: string;
+  actor?: ActivityObjectType;
+  object?: ActivityObjectType;
+  extra?: any;
+};
+
+export class StackClient {
+  config: ClientConfig;
+
+  constructor(config: ClientConfig) {
+    this.validateConfig(config);
+    this.config = config;
+  }
+
+  async track(
+    type: string,
+    activity?: Activity
+  ): Promise<LoggableActivityType> {
+    this.validateActivityType(type);
+
+    if (activity) {
+      this.validateActivity(activity);
+    }
+
+    const published = this.getCurrentTime();
+
+    const loggableActivity: LoggableActivityType = {
+      type,
+      published,
+      ...activity,
+    };
+
+    try {
+      await this.logActivity(loggableActivity);
+    } catch (e) {
+      // Just log to the console.
+      console.error(e);
+    }
+
+    return loggableActivity;
+  }
+
+  private async logActivity(loggableActivity: LoggableActivityType) {
+    const logResponse = await fetch(STACK_API_ENDPOINTS.TRACK, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.config.key}`,
+      },
+      body: JSON.stringify(loggableActivity),
+    });
+
+    const logResult = await logResponse.json();
+
+    const { success } = logResult;
+    assert(success, STACK_CLIENT_ERRORS.UNEXPECTED_SERVER_RESPONSE);
+  }
+
+  private getCurrentTime() {
+    const published = Date.now();
+    return published;
+  }
+
+  private validateActivity(activity: Activity) {
+    const VALID_ACTIVITIES = ['actor', 'object'];
+    const activityKeys = Object.keys(activity);
+    assert(
+      activityKeys.length <= VALID_ACTIVITIES.length,
+      STACK_CLIENT_ERRORS.INVALID_ACTIVITY_PROVIDED
+    );
+    for (let i = 0; i < activityKeys.length; i++) {
+      assert(
+        VALID_ACTIVITIES.includes(activityKeys[i]),
+        STACK_CLIENT_ERRORS.INVALID_ACTIVITY_PROVIDED
+      );
+    }
+  }
+
+  private validateActivityType(type: string) {
+    assert(Boolean(type), STACK_CLIENT_ERRORS.NO_ACTIVITY_TYPE_PROVIDED);
+    assert(
+      typeof type === 'string',
+      STACK_CLIENT_ERRORS.INVALID_ACTIVITY_TYPE_PROVIDED
+    );
+  }
+
+  private validateConfig(config: ClientConfig) {
+    assert(Boolean(config.key), STACK_CLIENT_ERRORS.NO_API_KEY_PROVIDED);
+    assert(typeof config.key === 'string', STACK_CLIENT_ERRORS.INVALID_API_KEY);
+  }
+}
+
+export function initStack(key: string) {
+  const stack_ = new StackClient({ key });
+  return stack_;
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,169 @@
-import { myPackage } from '../src';
+import {
+  initStack,
+  LoggableActivityType,
+  STACK_API_ENDPOINTS,
+  STACK_CLIENT_ERRORS,
+  StackClient,
+} from '../src';
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  describe('Client', () => {
+    const validKey = '123';
+    // This is invalid, because it's an object containing the key.
+    const invalidKey = { key: validKey };
 
-      const result = myPackage(message);
+    describe('constructor', () => {
+      describe('when no key is given', () => {
+        it(`should throw this error: \n\t\t${STACK_CLIENT_ERRORS.NO_API_KEY_PROVIDED}`, () => {
+          expect(() => {
+            // @ts-ignore
+            initStack();
+          }).toThrow(STACK_CLIENT_ERRORS.NO_API_KEY_PROVIDED);
+        });
 
-      expect(result).toMatch(message);
+        describe('when an invalid key is given', () => {
+          it(`should throw this error: \n\t\t${STACK_CLIENT_ERRORS.INVALID_API_KEY}`, () => {
+            expect(() => {
+              // @ts-ignore
+              initStack(invalidKey);
+            }).toThrow(STACK_CLIENT_ERRORS.INVALID_API_KEY);
+          });
+        });
+
+        describe('when given a valid key', () => {
+          it('should not throw', () => {
+            expect(() => {
+              initStack(validKey);
+            }).not.toThrow(STACK_CLIENT_ERRORS.INVALID_API_KEY);
+          });
+
+          it('should return a valid client with the key set', () => {
+            const client = initStack(validKey);
+
+            expect(client).toMatchObject({ config: { key: validKey } });
+          });
+        });
+      });
+
+      describe('track', () => {
+        const validType = 'CONNECT_WALLET';
+        let client: StackClient;
+
+        beforeAll(() => {
+          client = initStack(validKey);
+        });
+
+        beforeEach(() => {
+          // @ts-ignore
+          global.fetch = jest.fn(() => {
+            return Promise.resolve({
+              json: () => Promise.resolve({ success: true }),
+            });
+          });
+        });
+
+        describe('when not given any type', () => {
+          it(`throws this error: ${STACK_CLIENT_ERRORS.NO_ACTIVITY_TYPE_PROVIDED}`, async () => {
+            await expect(async () => {
+              // @ts-ignore
+              await client.track();
+            }).rejects.toThrow(STACK_CLIENT_ERRORS.NO_ACTIVITY_TYPE_PROVIDED);
+          });
+        });
+
+        describe('when given an invalid type', () => {
+          it(`throws this error: ${STACK_CLIENT_ERRORS.INVALID_ACTIVITY_TYPE_PROVIDED}`, async () => {
+            await expect(async () => {
+              // @ts-ignore
+              await client.track(123);
+            }).rejects.toThrow(
+              STACK_CLIENT_ERRORS.INVALID_ACTIVITY_TYPE_PROVIDED
+            );
+          });
+        });
+
+        describe('when given a valid type', () => {
+          let response: LoggableActivityType;
+
+          beforeEach(async () => {
+            response = await client.track(validType);
+          });
+
+          it('returns the full activity to log', () => {
+            expect(response.type === validType);
+            expect(typeof response.published === 'number');
+          });
+
+          it('sends the data to the API', () => {
+            // Only gets called once.
+            expect(fetch).toHaveBeenCalledTimes(1);
+            // Gets called with the expected properties.
+            expect(fetch).toHaveBeenCalledWith(STACK_API_ENDPOINTS.TRACK, {
+              body: JSON.stringify(response),
+              headers: {
+                Authorization: `Bearer ${validKey}`,
+                'Content-Type': 'application/json',
+              },
+              method: 'POST',
+            });
+          });
+        });
+
+        describe('when given a valid type and invalid activity', () => {
+          it(`throws this error: ${STACK_CLIENT_ERRORS.INVALID_ACTIVITY_PROVIDED}`, async () => {
+            await expect(async () => {
+              await client.track(validType, {
+                // @ts-ignore
+                nothing: 'good will come of this',
+              });
+            }).rejects.toThrow(STACK_CLIENT_ERRORS.INVALID_ACTIVITY_PROVIDED);
+          });
+        });
+
+        describe('when given a valid type and an actor and an object', () => {
+          const validType = 'CONNECT_WALLET';
+          const actor = {
+            type: 'wallet',
+            address: '0x375892Bb243D35E4c12e8a87b95e24F7F53d493E',
+          };
+
+          const object = {
+            type: 'contract',
+            address: '0xFB72CD75d0b9022810F7748Dd36D767836FBcBDE',
+            id: 3,
+          };
+
+          let response: LoggableActivityType;
+
+          beforeEach(async () => {
+            response = await client.track(validType, {
+              actor,
+              object,
+            });
+          });
+
+          it('returns the full activity to log', () => {
+            expect(response.type === validType);
+            expect(typeof response.published === 'number');
+            expect(response.actor).toMatchObject(actor);
+            expect(response.object).toMatchObject(object);
+          });
+
+          it('sends the data to the API', () => {
+            // Only gets called once.
+            expect(fetch).toHaveBeenCalledTimes(1);
+            // Gets called with the expected properties.
+            expect(fetch).toHaveBeenCalledWith(STACK_API_ENDPOINTS.TRACK, {
+              body: JSON.stringify(response),
+              headers: {
+                Authorization: `Bearer ${validKey}`,
+                'Content-Type': 'application/json',
+              },
+              method: 'POST',
+            });
+          });
+        });
+      });
     });
   });
 });
